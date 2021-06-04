5db66e788be04bb5cb5045c03a49d4d5f079bda0
vasanthigopisetti <84200561+vasanthigopisetti@users.noreply.github.com>
2021-06-04 16:24:57 +0530

    Add files via upload

---
+++ list_2.py
@@ -0,0 +1,4 @@
+n=int(input())
+data=[None for i in range(n)]
+
+
---
+++ list_3.py
@@ -0,0 +1,9 @@
+n=int(input())
+data=[0 for i in range(n)]
+
+for i in range(n):
+    val=int(input())
+    data[i]=val
+print(data)
+
+
