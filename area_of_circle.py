245ed8ca1fa617c53c89b855d856e542b3bf5667
vasanthigopisetti <84200561+vasanthigopisetti@users.noreply.github.com>
2021-06-04 16:45:13 +0530

    Add files via upload

---
+++ area_of_circle.py
@@ -0,0 +1,8 @@
+r=float(input())
+dia=2*r
+cir=2*3.14*r
+area=3.14*r**2
+print("%.2f"%area)
+print("%.2f"%cir)
+print("%.2f"%dia)
+      
---
+++ break.py
@@ -0,0 +1,15 @@
+num=int(input())
+i=1
+while i<=num:
+    print(i,end="")
+    i+=1
+else:
+    print("vasanthi")
+
+
+    """
+    if i==5:
+      break
+    i+=1
+print("hai")
+"""
